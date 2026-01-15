d1426ec3b6ac5dc585067fd4284ca2f5ae745ba2
bishal-svg <bishalbaibhav40@gmail.com>
2026-01-15 11:35:19 +0545

    baisc server with route

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ const express=require("express")
 const path =require("path")
 const app=express()
 
-//because of using static public folder will server html pages a static server no need to assign the route
-
-app.use(express.static(path.join(__dirname,"public")))
 
 
+let product={
+  cycle:{
+    category:"vechicle",
+    prize:"999"
+  },
+  watch:{
+      category:"electronics",
+      prize:"444"
+  }
+}
 
-/*
 app.get("/",(req,res)=>{
-  return res.sendFile(path.join("index.html"))
+  res.json(product)
 })
 
-app.get("/about",(req,res)=>{
- return res.sendFile(path.join("abouts.html"))
-})
-app.get("/contact",(req,res)=>{
- return res.sendFile(path.join("contactus.html"))
-})*/
+
+
+
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
